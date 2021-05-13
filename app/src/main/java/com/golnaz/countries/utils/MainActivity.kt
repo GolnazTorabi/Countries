d73d4bb589260c583c84a0b82d821d8b954fc113
golnaz <g.torabi76@gmail.com>
2021-05-13 18:25:47 +0430

    add application to manifest and add graph to main activity

--- app/src/main/java/com/golnaz/countries/utils/MainActivity.kt
+++ app/src/main/java/com/golnaz/countries/utils/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.golnaz.countries
+package com.golnaz.countries.utils
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.golnaz.countries.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
